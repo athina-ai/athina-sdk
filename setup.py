983ce888d26a89cebd6eb9dd10e9e186025e22ac
Akshat Gupta <akshatwillrocku@gmail.com>
2023-07-03 10:41:04 +0530

    add required packages

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="magik_prompt_sdk",
-    version="0.0.1",
+    version="0.0.2",
     author="Magik Labs App Team",
     author_email="hello@magiklabs.app",
     description="SDK to interact with LLM model APIs along with prompt management and versioning",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'openai',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
