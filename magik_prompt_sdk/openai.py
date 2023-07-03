88e4b0a09e1337a6bee0c1343d5b3449ca282cb1
Akshat Gupta <akshatwillrocku@gmail.com>
2023-07-03 10:15:15 +0530

    add chat completion and completion methods

---
+++ magik_prompt_sdk/openai.py
@@ -0,0 +1,44 @@
+import requests
+import openai
+import json
+
+GPT3_MODELS = ["text-davinci-003", "text-davinci-002"]
+
+
+class OpenAI:
+    def __init__(self, API_KEY):
+        self.openai = openai
+        self.openai.api_key = API_KEY
+
+    def chat_completion(self, model, prompt):
+        """
+        Call the OpenAI API to generate a chat completion for models like gpt3.5 turbo and gpt 4
+        """
+        promptResponse = requests.post(
+            "http://localhost:9000/api/v1/prompt", data={"text": prompt}).json()
+        completion = self.openai.ChatCompletion.create(
+            model=model, messages=[{"role": "user", "content": prompt}])
+        self.save_prompt_run_chat_completion(model, completion, promptResponse)
+        return completion
+
+    def completion(self, model, prompt):
+        """
+        Call the OpenAI API to generate a completion for models like davinci-003 and davinci-002
+        """
+        promptResponse = requests.post(
+            "http://localhost:9000/api/v1/prompt", data={"text": prompt}).json()
+        completion = self.openai.Completion.create(model=model, prompt=prompt)
+        self.save_prompt_run_completion(model, completion, promptResponse)
+        return completion
+
+    def save_prompt_run_chat_completion(self, model, completion, promptResponse):
+        """
+        Save prompt run to the database
+        """
+        requests.post("http://localhost:9000/api/v1/promptRun", json={"promptId": promptResponse["data"]["prompt"]["id"], "languageModelId": model,"promptSent": promptResponse["data"]["prompt"]["text"],"promptResponse": completion.choices[0].message.content, "tokensUsed": int(completion.usage.total_tokens)})
+        # requests.post("http://localhost:9000/api/v1/promptRun", json={"promptId": promptResponse["data"]["prompt"]["id"], "languageModelId": completion.model,"promptSent": promptResponse["data"]["prompt"]["text"],"promptResponse": completion.choices[0].text, "tokensUsed": int(completion.usage.total_tokens)})
+    def save_prompt_run_completion(self, model, completion, promptResponse):
+        """
+        Save prompt run to the database
+        """
+        requests.post("http://localhost:9000/api/v1/promptRun", json={"promptId": promptResponse["data"]["prompt"]["id"], "languageModelId": model,"promptSent": promptResponse["data"]["prompt"]["text"],"promptResponse": completion.choices[0].text, "tokensUsed": int(completion.usage.total_tokens)})
