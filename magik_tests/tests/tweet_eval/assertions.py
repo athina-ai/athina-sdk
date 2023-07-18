c3936c9645cd791480e4257d81d7144a57213eb0
Shiv Sakhuja <shivsak.s@gmail.com>
2023-07-19 00:53:34 +0530

    Updated CLI with run_prod command + updated run in production method for run.py + minor changes

--- magik_tests/tests/tweet_eval/assertions.py
+++ magik_tests/tests/tweet_eval/assertions.py
@@ -5,50 +5,81 @@ from magik.evaluators import (
     not_contains_pii,
     is_positive_sentiment,
     length_less_than,
+    contains,
+    negate,
 )
+
+
+def is_hallucination(output):
+    return {"result": False, "reason": "Custom reason for is_hallucination"}
+
 
 # Define tests here
 tests = [
     {
+        # not_contains_pii(output)
         "description": "output does not contain pii",
         "eval_function": not_contains_pii,
         "eval_function_args": [],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Ola!",
+            "num_chars": 280,
+        },
         "failure_labels": ["sensitive_data"],
     },
     {
+        # contains_link(output)
         "description": "output contains a link",
         "eval_function": contains_link,
         "eval_function_args": [],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Uber",
+            "num_chars": 280,
+        },
         "failure_labels": ["bad_response_format"],
     },
     {
+        # contains_valid_link(output)
         "description": "output contains a valid link",
         "eval_function": contains_valid_link,
         "eval_function_args": [],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Magik",
+            "num_chars": 280,
+        },
         "failure_labels": ["bad_response_format"],
     },
     {
+        # is_positive_sentiment(output)
         "description": "output sentiment is positive",
         "eval_function": is_positive_sentiment,
         "eval_function_args": [],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Lyft",
+            "num_chars": 280,
+        },
         "failure_labels": ["negative_sentiment"],
     },
     {
+        # length_less_than(output, 280)
         "description": "output length is less than 280 characters",
         "eval_function": length_less_than,
         "eval_function_args": [280],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Facebook",
+            "num_chars": 280,
+        },
         "failure_labels": ["negative_sentiment", "critical"],
     },
     {
+        # contains_all(output, ["#"])
         "description": "output does not contain hashtags",
         "eval_function": contains_none,
         "eval_function_args": ["#"],
-        "prompt_vars": {},
+        "prompt_vars": {
+            "name": "Datadog",
+            "num_chars": 280,
+        },
         "failure_labels": ["bad_response_format"],
     },
 ]
